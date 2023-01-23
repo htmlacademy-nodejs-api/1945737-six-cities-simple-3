a5439682935980e19334ff3f55db3b8c036d0dc8
olga-surovtseva <olga.surovtseva.94@mail.ru>
2023-01-24 00:04:55 +0500

    код для получения данных по команде generate

---
+++ src/cli-command/generate-command.ts
@@ -0,0 +1,28 @@
+import { appendFile } from 'fs/promises';
+import got from 'got';
+import { MockData } from '../types/mock-data.type.js';
+import { CliCommandInterface } from './cli-command.interface.js';
+import OfferGenerator from '../common/offer-generator/offer-generator.js';
+
+export default class GenerateCommand implements CliCommandInterface {
+  public readonly name = '--generate';
+  private initialData!: MockData;
+
+  public async execute(...parameters: string[]): Promise<void> {
+    const [count, filepath, url] = parameters;
+    const offerCount = Number.parseInt(count, 10);
+
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      return console.log(`Can't fetch data from ${url}.`);
+    }
+
+    const OfferGeneratorString = new OfferGenerator(this.initialData);
+
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(filepath, `${OfferGeneratorString.generate()}\n`, 'utf8');
+    }
+    console.log(`File ${filepath} was created!`);
+  }
+}
