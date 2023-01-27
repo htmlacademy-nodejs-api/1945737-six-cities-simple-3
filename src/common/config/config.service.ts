a50f1a165691989a15ff86b8d291c512576f181b
olga-surovtseva <olga.surovtseva.94@mail.ru>
2023-01-27 19:42:02 +0500

    добавила внедрение зависимостей

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -2,12 +2,16 @@ import {ConfigInterface} from './config.interface.js';
 import {config} from 'dotenv';
 import {configSchema, ConfigSchema} from './config.schema.js';
 import {LoggerInterface} from '../logger/logger.interface.js';
+import { inject, injectable } from 'inversify';
+import { Component } from '../../types/component.types.js';
+
+@injectable()
 
 export default class ConfigService implements ConfigInterface {
   private config: ConfigSchema;
   private logger: LoggerInterface;
 
-  constructor(logger: LoggerInterface) {
+  constructor(@inject(Component.LoggerInterface) logger: LoggerInterface) {
     this.logger = logger;
 
     const parsedOutput = config();
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import 'reflect-metadata';
 import Application from './app/application.js';
 import LoggerService from './common/logger/logger.service.js';
 import ConfigService from './common/config/config.service.js';
+import { Container } from 'inversify';
+import { LoggerInterface } from './common/logger/logger.interface.js';
+import { Component } from './types/component.types.js';
+import { ConfigInterface } from './common/config/config.interface.js';
 
-const logger = new LoggerService();
-const config = new ConfigService(logger);
+const applicationContainer = new Container();
+applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
+applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
+applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
 
-const application = new Application(logger, config);
+const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
